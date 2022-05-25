a881d4f15cfb68a49d86ebc6447423e66731e202
Gideon Odiokine <61180360+GideonOdiokine@users.noreply.github.com>
2022-05-25 02:46:17 +0100

    added Person Component

--- react-typescript-demo/src/components/Person.tsx
+++ react-typescript-demo/src/components/Person.tsx
@@ -1,9 +1,4 @@
-type PersonProps = {
-  name: {
-    firstName: string;
-    lastName: string;
-  };
-};
+import { PersonProps } from "./Person.types";
 
 export const Person = (props: PersonProps) => {
   return (
