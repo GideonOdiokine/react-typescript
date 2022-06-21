d3f5ebd8c75fe93e2a12114c67236f786be433c4
Gideon Odiokine <61180360+GideonOdiokine@users.noreply.github.com>
2022-06-21 05:00:50 +0100

    added GreetProps to GreetComponent

---
+++ react-typescript-demo/src/components/Greet.tsx
@@ -0,0 +1,19 @@
+type GreetProps = {
+  name: string;
+  messageCount: number;
+  isLoggedIn: boolean;
+};
+
+const Greet = (props: GreetProps) => {
+  return (
+    <div>
+      <h2>
+        {props.isLoggedIn
+          ? `Welcome ${props.name}! You have ${props.messageCount} unread messages`
+          : "Welcome guest"}
+      </h2>
+    </div>
+  );
+};
+
+export default Greet;
